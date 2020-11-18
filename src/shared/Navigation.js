510470ad3aa4ef4a06e2431a560658f4c5d2a0eb
chrispdharman <christopher.p.d.harman@gmail.com>
2020-11-18 16:48:16 +0000

    Fleshed out nav links

--- src/shared/Navigation.js
+++ src/shared/Navigation.js
@@ -1,4 +1,4 @@
-import logo from '../media/images/logo.svg';
+import logo from '../media/images/logo.png';
 import React from 'react';
 import './Navigation.css';
 
@@ -8,10 +8,35 @@ class Navigation extends React.Component {
   render() {
     return (
       <div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="Nav-logo" alt="logo" />
+
+        <a
+          className="Nav-link"
+          href="http://localhost:3000"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          About
+        </a>
+
+        <a
+          className="Nav-link"
+          href="http://localhost:3000"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Upload
+        </a>
+
+        <a 
+          className="Nav-link"
+          href="http://localhost:3000"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Manage
+        </a>
+
         <a
           className="App-link"
           href="https://reactjs.org"
