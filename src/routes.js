8ffc7a2d486b80495bdc80bdceaeaff1076e899c
chrispdharman <christopher.p.d.harman@gmail.com>
2020-11-20 11:42:46 +0000

    Add a landing page

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,11 @@ import { Switch } from 'react-router-dom';
 import About from './views/About';
 import ClearCut from './views/ClearCut';
 import Manage from './views/Manage';
+import Landing from './views/Landing';
 
 const routes = (
   <Switch>
+    <Route path='/' component={Landing} />
     <Route path='/about' component={About} />
     <Route path='/manage' component={Manage} />
     <Route path='/upload' component={ClearCut} />
---
+++ src/views/Landing.js
@@ -0,0 +1,17 @@
+import Navigation from '../shared/Navigation';
+import './Landing.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navigation />
+      </header>
+      <div className='App-body'>
+        <p>Landing page</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
