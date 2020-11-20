78382e439ecc148d67204df732092be24a837d6b
chrispdharman <christopher.p.d.harman@gmail.com>
2020-11-20 10:21:12 +0000

    Make about endpoint (not welcome)

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router';
 
+import About from './About';
 import App from './App';
 import ClearCut from './ClearCut';
-import Welcome from './Welcome';
 
 const routes = (
   <Route path='/' component={App}>
-    <Route component={Welcome} />
+    <Route path='/about' component={About} />
     <Route path='/clear-cut' component={ClearCut} />
   </Route>
 );
