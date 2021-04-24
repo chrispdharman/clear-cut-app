428570ae2808e0b5ff43d7e8e82ccbe652cb250d
chrispdharman <christopher.p.d.harman@gmail.com>
2021-04-24 11:29:49 +0100

    Test commit

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -10,7 +10,7 @@ function App() {
         <Navigation />
       </header>
       <div className='App-body'>
-        <p>Landing page</p>
+        <p>[Some funky animation or something here.]</p>
       </div>
     </div>
   );
