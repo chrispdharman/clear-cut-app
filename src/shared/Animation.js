d7e8c4950507c6569d84f47f1975485cd716fc56
chrispdharman <christopher.p.d.harman@gmail.com>
2021-04-25 19:25:35 +0100

    Made crazy animation better

--- src/shared/Animation.js
+++ src/shared/Animation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import scissors from '../media/images/scissors.png';
 import './Animation.css';
 
 
@@ -8,21 +9,38 @@ class Animation extends React.Component {
   render() {
     return (
       <div>
+        <div className='CuttingCircle'></div>
         <div className='RotatingPane Pane1'>
-          <div className='RotatingObject StartObject'/>
-          <div className='RotatingObject LastObject'/>
+          <div className='RotatingObject StartObject'>
+            <img src={scissors} className="Scissors icon" alt="scissors icon" />
+          </div>
+          <div className='RotatingObject LastObject'>
+            <img src={scissors} className="Scissors icon" alt="scissors icon" />
+          </div>
         </div>
         <div className='RotatingPane Pane2'>
-          <div className='RotatingObject StartObject'/>
-          <div className='RotatingObject LastObject'/>
+          <div className='RotatingObject StartObject'>
+            <img src={scissors} className="Scissors icon" alt="scissors icon" />
+          </div>
+          <div className='RotatingObject LastObject'>
+            <img src={scissors} className="Scissors icon" alt="scissors icon" />
+          </div>
         </div>
         <div className='RotatingPane Pane3'>
-          <div className='RotatingObject StartObject'/>
-          <div className='RotatingObject LastObject'/>
+          <div className='RotatingObject StartObject'>
+            <img src={scissors} className="Scissors icon" alt="scissors icon" />
+          </div>
+          <div className='RotatingObject LastObject'>
+            <img src={scissors} className="Scissors icon" alt="scissors icon" />
+          </div>
         </div>
         <div className='RotatingPane Pane4'>
-          <div className='RotatingObject StartObject'/>
-          <div className='RotatingObject LastObject'/>
+          <div className='RotatingObject StartObject'>
+            <img src={scissors} className="Scissors icon" alt="scissors icon" />
+          </div>
+          <div className='RotatingObject LastObject'>
+            <img src={scissors} className="Scissors icon" alt="scissors icon" />
+          </div>
         </div>
       </div>
     );
