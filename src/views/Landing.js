711f49242380cc9a008841ed5370512e3300e013
chrispdharman <christopher.p.d.harman@gmail.com>
2021-04-26 21:24:02 +0100

    Fleshed out and styled the landing page

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -11,11 +11,17 @@ function App() {
         <Navigation />
       </header>
       <div className='App-body'>
-        <p>Welcome to Chris' Clear Cut website! <br /> <br />
-          If you ever seen the below logo it means something is procesing/loading. <br />
-          To stop the processing event, click the cancel button inside the animation.
+        <p className='LandingText'>
+          Welcome to Chris' Clear Cut website! <br /> <br />
+          If you ever see the below icon it means something is loading or being processed. <br /> <br />
+          To stop any loading/processing events, click the cancel button inside the animation. <br /> <br />
+          Feel free to try this now:
         </p>
         <Animation showCancel/>
+        <p className='LandingText'>
+          This site is not affliated to a company or organisation. <br /> <br />
+          It is simply the virtual playground of a bored and nerdy human being &#128406;.
+        </p>
       </div>
     </div>
   );
