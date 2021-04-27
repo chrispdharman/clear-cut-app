eb263dd08fe89969bf49fc91f2e8fde6ba597989
chrispdharman <christopher.p.d.harman@gmail.com>
2021-04-27 21:59:56 +0100

    Adding About section (WIP on arrows connecting images)

--- src/views/About.js
+++ src/views/About.js
@@ -1,4 +1,9 @@
 import Navigation from '../shared/Navigation';
+
+import original_image from '../media/images/0001_size_reduced_image.png';
+import phase_one_image from '../media/images/0006_merged_image_cut.png';
+import phase_two_image from '../media/images/0007_noise_reduced_image.png';
+import final_image from '../media/images/0008_edge_masked_image.png';
 import './Manage.css';
 
 function App() {
@@ -8,7 +13,19 @@ function App() {
         <Navigation />
       </header>
       <div className='App-body'>
-        <p>Welcome to ClearCut! An app designed to cut out the objects from any media you provide it.</p>
+        <p>
+          Welcome to ClearCut! An app designed to cut out the objects from any media you provide it. <br /> <br />
+          Best use cases include converting a whiteboard full of text into a tidy image with noise removed.
+        </p>
+        <div className='WhiteboardExample'>
+          <img src={original_image} className="Nav-logo" alt="original image" />
+          <p className='ImageConnector'><i className="arrow right"></i></p>
+          <img src={phase_one_image} className="Nav-logo" alt="gradient image" />
+          <p className='ImageConnector'><i className="arrow right"></i></p>
+          <img src={phase_two_image} className="Nav-logo" alt="noise reduced gradient image" />
+          <p className='ImageConnector'><i className="arrow right"></i></p>
+          <img src={final_image} className="Nav-logo" alt="final masked image" />
+        </div>
       </div>
     </div>
   );
